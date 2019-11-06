b37e60eb97159c6176f24562351c4da4398b6843
Pavel Ershov <pershov@game-insight.com>
2019-11-06 11:18:07 +0300

    New test

--- workers_1/index.js
+++ workers_1/index.js
@@ -1,18 +1,47 @@
 "use strict";
 
-const { fork } = require("child_process");
-
-const worker = fork("./worker");
-
-worker.on("message", (result) => {
-    res.send("Hello Intense ${fruit}!");
-    worker.kill();
-});
-
-worker.send("testMessage");
+//const child_process = require("child_process");
+const workerThreads = require("worker_threads");
 
 
-function listDirs(dirsToVisit, maxAtOnce) {
+/*function runChildProcess(){
+    const worker = child_process.fork("./worker");
+    worker.on("message", (result) => {
+        res.send("Hello Intense ${fruit}!");
+        worker.kill();
+    });
+    worker.send("testMessage");    
+}
+runChildProcess();*/
+
+function runThread(){
+    console.log(__dirname);
+
+    const workerOptions = {
+        workerData: {data: "workerCreateData"}
+    };
+
+    const worker = new workerThreads.Worker("./worker.js", workerOptions);
+    worker.on("online", ()=>{
+        console.log("Worker is online");
+    });
+    worker.on("message", (data)=>{
+        console.log("Message from worker");
+    });
+    worker.on("error", (err)=>{
+        console.log("Error from worker:", err);
+    });
+    worker.on("exit", (exitCode) =>{
+        console.log("Exit from worker");
+    });
+
+    worker.postMessage("test message 1");
+    worker.postMessage("test message 2");
+}
+
+runThread();
+
+/*function listDirs(dirsToVisit, maxAtOnce) {
     let numRunning = 0;
     let index = 0;
 
@@ -35,4 +64,4 @@ function listDirs(dirsToVisit, maxAtOnce) {
         }
     }
     runMore();
-}
+}*/
--- workers_1/worker.js
+++ workers_1/worker.js
@@ -1,8 +1,30 @@
 "use strict";
 
-const { sleep } = require('sleep');
+const workerThreads = require("worker_threads")
 
-process.on("message", (message) => {
-    sleep(5);
-    process.send( "Answer" );
-});
+if(workerThreads.isMainThread == false){
+    const workerCreationData = workerThreads.workerData;
+    console.log("Worker creation data: ", workerCreationData);
+
+    var totalReceivedMessages = 0;
+
+    if(workerThreads.parentPort !== null){
+        console.log("Worker port setup");
+        workerThreads.parentPort.on("message", (data)=>{
+            console.log("Received message in worker:", data)
+
+            workerThreads.parentPort.postMessage("Received data");
+
+            totalReceivedMessages += 1;
+
+            if (totalReceivedMessages >= 2){
+                workerThreads.parentPort.close();
+            }
+        });
+        workerThreads.parentPort.on("close", ()=>{
+            console.log("Port closed")
+        });
+    }else{
+        console.log("Worker NO port");
+    }
+}
