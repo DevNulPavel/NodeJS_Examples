c5b271609158958d6b9f1fb792df285885d463cb
Pavel Ershov <pershov@game-insight.com>
2019-11-08 12:58:39 +0300

    New commentaries

--- Test5_promises/06_generators_simple/index.js
+++ Test5_promises/06_generators_simple/index.js
@@ -1,12 +1,15 @@
 "use strict";
 
+// Функция-генератор помечается *
 function* fruitGenerator() {
   yield 'apple';
   yield 'orange';
   return 'watermelon';
 }
 
+// У генератора можно вызывать next до тех пор, пока второй параметр done не true
 const newFruitGenerator = fruitGenerator();
-console.log(newFruitGenerator.next());    //[1]
-console.log(newFruitGenerator.next());    //[2]
-console.log(newFruitGenerator.next());    //[3]
+console.log(newFruitGenerator.next()); // { value: 'apple', done: false }
+console.log(newFruitGenerator.next()); // { value: 'orange', done: false }
+console.log(newFruitGenerator.next()); // { value: 'watermelon', done: true }
+console.log(newFruitGenerator.next()); // { value: undefined, done: true }
--- Test5_promises/08_generators_passing_values/index.js
+++ Test5_promises/08_generators_passing_values/index.js
@@ -1,10 +1,11 @@
 "use strict";
 
+// Генератор так же может получать значения снаружи
 function* twoWayGenerator() {
   const what = yield null;
   console.log('Hello ' + what);
 }
 
 const twoWay = twoWayGenerator();
-twoWay.next();
-twoWay.next('world');
+twoWay.next();        // Сначала включаем итерирование по генератору
+twoWay.next('world'); // Затем пробрасываем значения внутрь
