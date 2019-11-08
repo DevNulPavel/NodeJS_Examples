5a506ca36dfcd6d5f863b7a24292afffbf226112
Pavel Ershov <pershov@game-insight.com>
2019-11-08 12:37:19 +0300

    New commentaries

--- Test5_promises/02_promises_sequential_execution/index_async_await.js
+++ Test5_promises/02_promises_sequential_execution/index_async_await.js
@@ -37,19 +37,54 @@ function spiderLinks(currentUrl, body, nesting) {
         promise = promise.then(nextLinkSpiderCb);
     };
     links.forEach(linkCb);
-    
     // Возвращаем получившуюся цепочку загрузки ссылок
     return promise;*/
     
     // Параллельный вариант
-    let promises = [];
+    /*let promises = [];
     links.forEach((link) =>{
         console.log(link);
         const prom = spider(link, nesting-1);
         promises.push(prom);
     });
+    return Promise.all(promises);*/
 
-    return Promise.all(promises);
+    // Параллельный вариант с ограничением
+    const MAX_PARALLEL_COUNT = 10;
+    // Набор активных в данный момент задач
+    let activePromises = new Set();
+    // Получим коллбек для вызова оповещения о конце всех действий
+    let resolveCb;
+    let finishPromise = new Promise((resolve)=>{
+        resolveCb = resolve;
+    });
+    // Лямбда запуска новых задач
+    const runNewPromise = ()=>{
+        // Если все задач окончились, то просто дожидаемся завершения
+        if(links.length == 0){
+            Promise.all(activePromises).then(resolveCb);
+            return;
+        }
+        // Если превышен порог одновременных задач - не стартуем новые
+        if(activePromises.size >= MAX_PARALLEL_COUNT){
+            return;
+        }
+        // Вынимаем новую ссылку
+        const link = links.pop();
+        // Получаем Promise
+        let spiderProm = spider(link, nesting - 1);
+        // Добавляем к списку активных
+        activePromises.add(spiderProm);
+        // При окончании удаляем из списка и пробуем стартануть новую задачу
+        spiderProm = spiderProm.then(()=>{
+            activePromises.delete(spiderProm);
+            runNewPromise();
+        });
+        // Пытаемся стартануть еще задачу
+        runNewPromise();
+    };
+    runNewPromise();
+    return finishPromise;
 }
 
 async function download(url, filename) {
