3fe66294ea3d12cdf0c7a3b37c8bdc6d98938d45
DevNul <devnulpavel@gmail.com>
2019-11-11 21:59:49 +0300

    New examples

--- gplay_uploader/index.js
+++ gplay_uploader/index.js
@@ -1,42 +1,156 @@
 "use strict";
 
-//http://frontendcollisionblog.com/javascript/2015/12/26/using-nodejs-to-upload-app-to-google-play.html
-//https://googleapis.dev/nodejs/googleapis/latest/androidpublisher/classes/Resource$Edits$Apks-1.html#upload
-//https://stackoverflow.com/questions/48274009/cant-upload-apk-to-google-play-developer-via-publisher-api
+const fs = require("fs");
+// our handy library
+const google = require("googleapis");
 
-const googleapis = require("googleapis");
 
-const authClient = new googleapis.auth.OAuth2(
-    YOUR_CLIENT_ID,
-    YOUR_CLIENT_SECRET,
-    YOUR_REDIRECT_URL
-);
-  
-// generate a url that asks permissions for Blogger and Google Calendar scopes
+// see below in "Finding your secret.json" to find out how to get this
+const key = require('../../../secret.json');
+
+// any unique id will do; a timestamp is easiest
+const EDIT_ID = "" + (new Date().getTime());
+
+// editing "scope" allowed for OAuth2
 const scopes = [
-'https://www.googleapis.com/auth/blogger',
-'https://www.googleapis.com/auth/calendar'
+    'https://www.googleapis.com/auth/androidpublisher'
 ];
-  
-const url = oauth2Client.generateAuthUrl({
-    // 'online' (default) or 'offline' (gets refresh_token)
-    access_type: 'offline',
-  
-    // If you only need one scope you can pass it as a string
-    scope: scopes
-  });
 
 
+function startEdit(jwtClient, play) {
+    return new Promise(function(resolve, reject) {
+        // Авторизуемся
+        jwtClient.authorize(function(err, tokens) {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            
+            // Устанавливаем токен для работы
+            oauth2Client.setCredentials(tokens);
+            
+            const params = {
+                resource: {
+                    id: EDIT_ID,
+                    expiryTimeSeconds: 600 // this edit will be valid for 10 minutes
+                }
+            };
+            play.edits.insert(params, function(err, edit) {
+                if(err || !edit) {
+                    reject(err);
+                }
+                
+                const resolveData = {
+                    edit: edit
+                };
+                resolve(resolveData);
+            });
+        });
+    });
+}
 
-const publisher = googleapis.androidpublisher("v3");
+function upload(play, data) {
+    const edit = data.edit;
+    const apk = data.apk;
+    
+    return new Promise(function(resolve, reject) {
+        const uploadConfig = {
+            editId: edit.id,
+            media: {
+                mimeType: 'application/vnd.android.package-archive',
+                body: apk
+            }
+        };
+        play.edits.apks.upload(uploadConfig, function(err, res) {
+            if(err || !res) {
+                reject(err);
+            }
+            
+            // TODO: !!!
+            // pass any data we care about to the next function call
+            resolve(_.omit(_.extend(data, { uploadResults: res }), 'apk'));
+        });
+    });
+}
 
-const uploadParameters = {
-    auth: authClient, // API_KEY??
-    editId: {},
-    media: {},
-    packageName: "com.gameinsight."
-};
-const callback = ()=>{
+function setTrack(play, data) {
+    const edit = data.edit;
+    const track = tracks[argv[0] || 'alpha'];
+    
+    return new Promise(function(resolve, reject) {
+        play.edits.tracks.update({
+            editId: edit.id,
+            track: track,
+            resource: {
+                track: track,
+                versionCodes: [+data.uploadResults.versionCode]
+            }
+        }, function(err, res) {
+            if(err || !res) {
+                reject(err);
+            }
+            
+            resolve(_.extend(data, { setTrackResults: res }));
+        });
+    });
+    
+}
 
-};
-publisher.edits.apks.upload(uploadParameters, callback);
+function commitToPlayStore(play, data) {
+    return new Promise(function(resolve, reject) {
+        play.edits.commit({
+            editId: data.edit.id
+        }, function(err, res) {
+            if(err || !res) {
+                reject(err);
+            }
+            
+            resolve(_.extend(data, { commitToPlayStoreResults: res }));
+        });
+    });
+}
+
+function main(){
+    // here, we'll initialize our client
+    const oauth2Client = new google.auth.OAuth2();
+    const jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, scopes, null);
+    const play = google.androidpublisher({
+        version: 'v3',
+        auth: oauth2Client,
+        params: {
+            // default options
+            // this is the package name for your initial app you've already set up on the Play Store
+            packageName: 'com.example.app'
+        }
+    });
+
+    google.options({ 
+        auth: oauth2Client 
+    });
+
+    // "open" our edit
+    startEdit(jwtClient, play).then(function(data) {
+        //var apk = require('fs').readFileSync('./Chronicled.apk');
+        const apk = fs.createReadStream("file.apk");
+        
+        // stage the upload (doesn't actually upload anything)
+        return upload({
+            edit: data.edit,
+            apk: apk
+        });
+    }).then(function(data) {
+        // set our track
+        return setTrack(play, data);
+    }).then(function(data) {
+        // commit our changes
+        return commitToPlayStore(play, data);
+    }).then(function(data) {
+        // log our success!
+        console.log('Successful upload:', data);
+    }).catch(function(err) {
+        console.log(err);
+        process.exit(0);
+    });
+}
+
+main();
