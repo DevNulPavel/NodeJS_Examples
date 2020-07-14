e403164d9f00a4bcff9bcc153778261f27803218
Pavel Ershov <pershov@game-inisght.com>
2020-07-14 17:48:53 +0300

    SSH uploader fix

--- uploader_app/src/uploaders/ssh_uploader.ts
+++ uploader_app/src/uploaders/ssh_uploader.ts
@@ -60,6 +60,7 @@ export async function uploadBySSH(serverName: string, user: string, pass: string
         const onReadyFunc = async ()=>{
             let serverDir = paramServerDir;
             
+            //console.log("ssh on ready");
             try{
                 // Если путь относительно домашней папки, то надо обновить путь до абсолютного
                 if(serverDir.startsWith("~")){
@@ -79,54 +80,130 @@ export async function uploadBySSH(serverName: string, user: string, pass: string
                     serverDir = serverDir.replace("~", homeFolderPath);
                 }
                 
+                //console.log("sftp");
+
+                /*const ok = client.sftp((err, sftp)=>{
+                    setTimeout(()=>{
+                        console.log(`sftp error: ${err}`);
+                        if (err) {
+                            throw err;
+                        }
+                        console.log(`sftp object: ${sftp}`);
+    
+                        // https://github.com/mscdex/ssh2-streams/blob/master/SFTPStream.md
+                        let uploadConfig = {
+                            concurrency: 1,
+                            chunkSize: 1024 * 4
+                        };
+                        if(progressCb){
+                            const fileUploadProgressCb = (totalTransfered, chunkSize)=>{
+                                progressCb(chunkSize);
+                            };
+                            uploadConfig["step"] = fileUploadProgressCb;
+                        }
+    
+                        let upload = (i)=>{
+                            if (i < filePaths.length) {
+                                const localFilePath = filePaths[i];
+                                const fileName = path.basename(localFilePath);
+                                const remoteFilePath = path.join(serverDir, fileName);
+                                
+                                console.log(`upload ${localFilePath}, ${fileName}, ${remoteFilePath}`);
+                                sftp.fastPut(localFilePath, remoteFilePath, uploadConfig, (err) => {
+                                    console.log(`${err}`);
+                                    
+                                    i += 1;
+                                    upload(i);
+                                });                                
+                            }else{
+                                resolve();
+                                client.end();
+                            }
+                        };
+                        upload(0);                        
+                    }, 500);
+                });
+                console.log(`Is ok: ${ok}`);*/
+
                 // Создание папки руками
                 //const execFunc = util.promisify(client.exec.bind(client));
                 //await execFunc(`mkdir -p ${serverDir}`);
-        
+
                 const sftpFunc = util.promisify(client.sftp.bind(client));
 
-                const sftp = await sftpFunc();
+                let sftp = undefined;
+                try{
+                    sftp = await sftpFunc();
+                }catch(err){
+                    reject(err);
+                    return;
+                }
+
                 const mkdirFunc = util.promisify(sftp.mkdir.bind(sftp));
-                const fastPutFunc = util.promisify(sftp.fastPut.bind(sftp));
+                const fastPutFunc = util.promisify(sftp.fastPut.bind(sftp));                
                 
                 // Создаем папку с помощью SFTP
                 try{
                     // Если папка уже есть, то OK
                     await mkdirFunc(serverDir);
+                    //console.log("ssh makedir");
                 }catch(err){
                 }
 
-                let uploadConfig = undefined;
+                // https://github.com/mscdex/ssh2-streams/blob/master/SFTPStream.md
+                let uploadConfig = {
+                    concurrency: 8,
+                    chunkSize: 1024 * 32
+                };
                 if(progressCb){
                     const fileUploadProgressCb = (totalTransfered, chunkSize)=>{
                         progressCb(chunkSize);
                     };
-                    uploadConfig = { 
-                        step: fileUploadProgressCb 
-                    };
+                    uploadConfig["step"] = fileUploadProgressCb;
                 }
+
+                //console.log("ssh upload");
         
                 for(let i = 0; i < filePaths.length; i++){
                     const localFilePath = filePaths[i];
                     const fileName = path.basename(localFilePath);
                     const remoteFilePath = path.join(serverDir, fileName);
+
+                    //console.log(`upload ${localFilePath}, ${fileName}, ${remoteFilePath}`);
+
                     await fastPutFunc(localFilePath, remoteFilePath, uploadConfig);
                 }
         
                 resolve();
-            }finally{
                 client.end();
+            }catch(err){
+                reject(err);
+                client.end();
+            }finally{
             }
         };
         client.on("ready", onReadyFunc);
         client.on("error", reject);
         client.on("timeout", reject);
+        client.on("continue", ()=>{
+            //console.log("ssh continue");
+        });
+        client.on("connect", ()=>{
+            //console.log("ssh connect");
+        });
         
         // Выполняем подключение
         const connectConfig: ssh2.ConnectConfig = {
             host: serverName,
             port: 22,
-            username: user
+            username: user,
+            keepaliveCountMax: 5,
+            compress: false,
+            keepaliveInterval: 100,
+            readyTimeout: 4000
+            /*debug: (text)=>{
+                console.log(text);
+            }*/
         };
         if(pass && (pass.length > 0)){
             connectConfig.password = pass;
